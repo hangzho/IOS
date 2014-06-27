c23f85badf5dd28e250abe24e778c297c66224e7
joe <johanzhou8@gmail.com>
2014-06-27 10:00:26 -0400

    Lynda.com iOS development video. simple interaction demo

---
+++ BasicInteraction/BasicInteraction/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  BasicInteraction
+//
+//  Created by Johan on 6/26/14.
+//  Copyright (c) 2014 Johan. All rights reserved.
+//
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBOutlet var simpleTextField : UITextField
+    @IBOutlet var simpleLabel : UILabel
+    @IBAction func changeLabel(sender : AnyObject) {
+        var contents = simpleTextField.text
+        simpleLabel.text = "Hello " + contents
+        
+        simpleTextField.resignFirstResponder()
+    }
+    override func touchesBegan(touches: NSSet!, withEvent event: UIEvent!) {
+        view.endEditing(true)
+    }
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        simpleTextField.resignFirstResponder()
+        return true
+    }
+
+}
+
