0ee25ee82304d16b338b0d2215e024f2d33af85a
joe <johanzhou8@gmail.com>
2014-06-27 12:42:03 -0400

    DatePicker

--- AutoLayout/AutoLayout/ViewController.swift
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -19,6 +19,17 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBOutlet var showLabel : UILabel
+    @IBOutlet var datePicker : UIDatePicker
+    @IBAction func displayDay(sender : AnyObject) {
+        var chosen:NSDate = datePicker.date
+        var formatter:NSDateFormatter = NSDateFormatter()
+        formatter.dateFormat = "EEEE"
+        var weekday = formatter.stringFromDate(chosen)
+        var msg = "this is a " + weekday
+        showLabel.text = msg
+        
+    }
 
 
 }
